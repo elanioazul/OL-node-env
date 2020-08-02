f1fcbe1a95059b4eb387045ceed1148e51c46cff
hugo <hugo.herrador@gmail.com>
2020-08-02 20:11:30 +0200

    basic controls-part1

--- ol-map.js
+++ ol-map.js
@@ -1,9 +1,10 @@
-import 'ol/ol.css'
-import {Map, View} from 'ol'
-import TileLayer from 'ol/layer/Tile'
-import OSM from 'ol/source/OSM'
-
-
+import 'ol/ol.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import * as olControl from 'ol/control';
+import {defaults} from 'ol/control';
+import FullScreen from 'ol/control/FullScreen';
 
 
 var osm = new TileLayer({
@@ -12,12 +13,29 @@ var osm = new TileLayer({
     maxZoom: 18
 })
 
-//Definimos la variable map que alojará nuestro mapa
-var map = new Map({
-    target: 'map',
-    layers: [osm],
-    view: new View({
-        center: [312807, 5156486],
-        zoom: 14
-    })
+var myview = new View({
+    center: [-414243.053976, 4929595.937425],
+    zoom: 14
 })
+
+var fullscreenbtn = new FullScreen({
+    className: 'fullscreen'
+
+})
+
+function init() {
+    //Definimos la variable map que alojará nuestro mapa
+
+
+    //al meter fullscreen btn he perdido los controles default zoom y attributions!!! mirar pqué!
+    var map = new Map({
+        target: 'map',
+        layers: [osm],
+        view: myview,
+        controls: [fullscreenbtn]
+    })
+}
+
+init();
+
+
