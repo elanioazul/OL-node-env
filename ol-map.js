9e4d3d3602306e32b17b8f2bceb917b804537593
hugo <hugo.herrador@gmail.com>
2020-08-05 14:06:28 +0200

    Add switchlayer control by matt walker

--- ol-map.js
+++ ol-map.js
@@ -1,25 +1,81 @@
 import 'ol/ol.css';
-import {Map, View} from 'ol';
+import { Map, View, Tile } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import Stamen from 'ol/source/Stamen';
 
 import FullScreen from 'ol/control/FullScreen';
-import {defaults as defaultControls, OverviewMap} from 'ol/control.js'
+import { defaults as defaultControls, OverviewMap } from 'ol/control.js';
 
-//LAYERS------------------ STUFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+import LayerSwitcher from 'ol-layerswitcher/src/ol-layerswitcher.js';
+import TileWMS from 'ol/source/TileWMS.js';
+import { Group as LayerGroup } from 'ol/layer.js'
+
+
+//---------------------------------------------------------------
+//LAYERS----------------------------------------------------------
+//--------------------------------------------------------------
+
+//served from third parties
 var osm = new TileLayer({
+    title: 'OSM',//Título de la capa
+    type: 'base',//Tipo de capa
+    visible: true,
     source: new OSM(),
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 18
 })
 
-//VIEW----------------- STUFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+var stamenlayer = new TileLayer({
+    title: 'Water color', //Título de la capa
+    type: 'base',//Tipo de capa
+    visible: false,
+    source: new Stamen({
+        layer: 'watercolor'
+    })
+})
+
+
+//switch layer var
+var mylayers = [
+    //Base maps
+    new LayerGroup({
+        'title': 'Base maps',
+        layers: [osm, stamenlayer]
+    })
+    ,
+    //Capa Overlay
+    new LayerGroup({
+        title: 'Overlays',
+        layers: [
+            new TileLayer({
+                title: 'Ortofoto',//Título de la capa
+                type: 'overlays',//Tipo de capa
+                opacity: 0.1,
+                source: new TileWMS({
+                    url: 'http://www.ign.es/wms/pnoa-historico?',
+                    params: { 'LAYERS': 'PNOA2004', 'TILED': true }
+                })
+            })
+        ]
+    })
+]
+
+//served from my own Geoserver
+
+
+
+//---------------------------------------------------------------------------
+//VIEW-----------------------------------------------------------------------
+//----------------------------------------------------------------------------
 var myview = new View({
     center: [-414243.053976, 4929595.937425],
     zoom: 14
 })
 
-//CONTROLS-------------- STUFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+//----------------------------------------------------------------------------------
+//CONTROLS---------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
 var fullscreenbtn = new FullScreen({
     className: 'fullscreen',
 })
@@ -37,16 +93,27 @@ var overview = new OverviewMap({
     tipLabel: 'Mapa de referencia'
 })
 
+
+var layerSwitcher = new LayerSwitcher({
+    tipLabel: 'Leyenda'
+})
+
+
+
+
 function init() {
     //Definimos la variable map que alojará nuestro mapa
     var map = new Map({
         target: 'map',
-        layers: [osm],
+        layers: mylayers,
         view: myview,
         controls: defaultControls().extend([
-            fullscreenbtn, overview
+            fullscreenbtn, overview, layerSwitcher
         ])
     })
+    //Agregamos el control al mapa
+    map.addControl(layerSwitcher)
+
 }
 
 init();
