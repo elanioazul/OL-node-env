ace66fbbd12e833e578006dc16fe8a00222ab0d4
hugo <hugo.herrador@gmail.com>
2020-08-13 10:13:48 +0200

    Add new sql query for coalesce null values

--- sql notebook.sql
+++ sql notebook.sql
@@ -6,3 +6,9 @@ WHERE shop =''
 RETURNING *;
 
 -- Plan es simbologia según field "shop" y luego sacar los overlays para los fields: amenity, brand, cuisine, name y shop
+
+-- Hago un back up dela bbdd.
+pg_dump -h localhost -U postgres -p 5432 tfm_unigis > tfm_unigis_backup.dump
+-- Me he dado cuenta que las zonas censales sin indice, salen null y no se representan en los estilos. Meto que sea un 0 en vez de null en aquellos poligonos que no tienen comercios
+CREATE TABLE indicadores_zonascenso_comercios_nonull AS
+	SELECT t1_1_indic AS habitantes, geom, objectid_zonas, COALESCE(nodes_count, 0) AS num_comercios, COALESCE(tfm_indice,0) AS ratio FROM indicadores_zonascenso_comercios;
